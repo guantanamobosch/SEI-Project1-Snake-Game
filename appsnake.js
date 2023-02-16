4c573b86aae26c4b23eb5811d427084dbd59ae60
guantanamobosch <luketyson17@gmail.com>
2023-02-16 14:00:52 -0700

    add event listener to recognize specific keystrokes and log them to the console

--- appsnake.js
+++ appsnake.js
@@ -1,10 +1,25 @@
-// Variables
+// ***Variables***
 
 
 // let's store the squares in some variables
 const squares = document.querySelectorAll('.square');
-console.log(squares);
+// console.log(squares);
 
-// Functions
+// ***Functions***
+// function myFunction(event) {
+//     let key = event.key;
+//     console.log(key);
+// }
 
-// Event Listeners
+// ***Event Listeners***
+// document.defaultView.addEventListener('onkeydown', function (event) {
+//     console.log("Hello world!");
+// })
+// Original attempt at logging a key press, did not work with 'onkeydown', which is probably older syntax
+
+
+document.addEventListener("keydown", function (event) {
+    console.log(event.key);
+})
+
+// this method of 'keydown' did work, and I got it directly from https://css-tricks.com/snippets/javascript/javascript-keycodes/ --- yeehaw
